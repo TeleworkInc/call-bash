0c3b40e2dda97a340f5167ea3dcb7c9d1ab6b6de
Christian Lewis <ctjl96@gmail.com>
2020-08-13 23:25:38 -0500

    Added annotations and newline printing

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
+/**
+ * @license MIT
+ */
+/**
+ * @fileoverview
+ * The entry point for this package.
+ *
+ * @author Christian Lewis
+ */
 const { spawn } = require('child_process');
 
 const DEFAULTS = {
-    shell: true,
-    stdio: 'inherit'
+  shell: true,
+  stdio: 'inherit',
 };
 
 /**
  * Execute a sequence of shell commands.
- * 
- * @param {string...} cmds
+ *
+ * @param {...string} cmds
  * The commands to run in sequential order, i.e. `shell('echo hello world',
  * 'echo 42')`.
- * 
+ *
  * @return {Promise}
  * A Promise that will resolve when call is finished, or reject on error.
  */
 const shell = async (...cmds) => {
-    for (let cmd of cmds) {
-        cmd = cmd.split(' ');
-        await new Promise((resolve, reject) => {
-            spawn(cmd.shift(), cmd, global.SPAWN_OPTIONS || DEFAULTS)
-            .on(
-                'exit', 
-                code => code == 0 
-                    ? resolve() 
-                    : reject()
-            );
-        });
-    }
-}
+  for (let cmd of cmds) {
+    cmd = cmd.split(' ');
+    await new Promise((resolve, reject) => {
+      spawn(cmd.shift(), cmd, global.SPAWN_OPTIONS || DEFAULTS)
+          .on(
+              'exit',
+              (code) => code == 0
+                    ? resolve()
+                    : reject(new Error('Exited with code: ' + code)),
+          );
+    });
+  }
+  /** Write newline to prevent visual clutter. */
+  console.log();
+};
 
 module.exports = shell;
